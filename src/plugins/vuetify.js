dd5996e03933a16ff1819e811528bd485a8e9f1f
Guilherme <guiferreiramoraes@outlook.com>
2025-06-01 01:21:19 -0300

    feat: vuetify initialization, import file input component

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,21 @@
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import { VFileUpload } from 'vuetify/labs/VFileUpload'
+
+export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+  theme: {
+    defaultTheme: 'light',
+  },
+	components: {
+		VFileUpload,
+	}
+})
+
